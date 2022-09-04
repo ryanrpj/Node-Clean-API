d2832808d569d2f3d18807051dc7485224299b69
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-09-04 18:04:23 -0300

    test: ensure BcryptAdapter returns hashed value on success

--- src/infra/criptography/bcryp-adapter.spec.ts
+++ src/infra/criptography/bcryp-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return 'hashed_value'
+  }
+}))
+
 describe('Bcrypt Adapter', function () {
   test('Should call bcrypt with correct value and salt', async () => {
     const salt = 12
@@ -9,5 +15,13 @@ describe('Bcrypt Adapter', function () {
 
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return hashed value on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+
+    const hashedValue = await sut.encrypt('any_value')
+    expect(hashedValue).toBe('hashed_value')
   })
 })
