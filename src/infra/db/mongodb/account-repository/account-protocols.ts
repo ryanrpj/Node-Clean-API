f813230f95ff927eac84a5e0b6fce2143566d7a0
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-09-15 05:22:38 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../../../data/protocols/add-account-repository'
+export * from '../../../../domain/usecases'
+export * from '../../../../domain/models'
---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,16 @@
+import { AccountModel, AddAccountModel, AddAccountRepository } from './account-protocols'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    const collection = MongoHelper.getCollection('accounts')
+    const { insertedId } = await collection.insertOne(account)
+    const insertedAccount = await collection.findOne({ _id: insertedId })
+
+    return {
+      id: insertedId.toString(),
+      name: insertedAccount?.name,
+      email: insertedAccount?.email
+    }
+  }
+}
