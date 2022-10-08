947cc4efeee684292aa0fb8342b736b0dd344c62
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-08 17:37:31 -0300

    refactor: connecting to mongodb before server starts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,9 @@
-import app from './config/app'
 import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 
-app.listen(env.serverPort, () => console.log(`Server running at http://localhost:${env.serverPort}.`))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.serverPort, () => console.log(`Server running at http://localhost:${env.serverPort}.`))
+  })
+  .catch(console.error)
