41e3084dea026a8006ad65d7252ee9e18f8cfbe2
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-08 22:47:27 -0300

    feat: add SignUpController factory

---
+++ src/main/factories/signup-controller.ts
@@ -0,0 +1,14 @@
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const encrypter = new BcryptAdapter(salt)
+  const addAccountRepository = new AccountMongoRepository()
+  const addAccount = new DbAddAccount(encrypter, addAccountRepository)
+  const emailValidator = new EmailValidatorAdapter()
+  return new SignUpController(emailValidator, addAccount)
+}
