5c397e425265ad0453176accbe3f76237ef86f2d
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-08 22:50:13 -0300

    test: signup route

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,10 +1,26 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import env from '../config/env'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => await MongoHelper.connect(env.mongoUrl))
+
+  afterAll(async () => await MongoHelper.disconnect())
+
+  beforeEach(async () => {
+    const collection = MongoHelper.getCollection('accounts')
+    await collection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
-      .expect(200)
+      .send({
+        name: 'Ryan Ribeiro',
+        email: 'test@ryanribeiro.dev',
+        password: '@RyanRPJ.'
+      })
+      .expect(201)
   })
 })
