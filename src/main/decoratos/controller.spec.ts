2872470a145982cb333014da8437b258dcda0c89
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-16 18:26:47 -0300

    test: ensure LogControllerDecorator calls controller with correct parameters

---
+++ src/main/decoratos/controller.spec.ts
@@ -0,0 +1,41 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { LogControllerDecorator } from './controller'
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controller: Controller
+}
+
+class ControllerStub implements Controller {
+  async handle (request: HttpRequest): Promise<HttpResponse> {
+    return {
+      statusCode: 200,
+      body: { ok: 'ok' }
+    }
+  }
+}
+
+const makeSut = (): SutTypes => {
+  const controller = new ControllerStub()
+  const sut = new LogControllerDecorator(controller)
+  return { controller, sut }
+}
+
+describe('Log Controller Decorator', () => {
+  test('Should call controller with correct parameters', async () => {
+    const { sut, controller } = makeSut()
+    const handleSpy = jest.spyOn(controller, 'handle')
+
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
