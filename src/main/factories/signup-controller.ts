146bf9ae59029f6d6447339589ba60eb3f1df5f7
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-17 17:34:54 -0300

    refactor: build SignUpController with LogControllerDecorator

--- src/main/factories/signup-controller.ts
+++ src/main/factories/signup-controller.ts
@@ -3,12 +3,17 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from '../decorators/controller'
+import { Controller } from '../../presentation/protocols'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const encrypter = new BcryptAdapter(salt)
   const addAccountRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(encrypter, addAccountRepository)
   const emailValidator = new EmailValidatorAdapter()
-  return new SignUpController(emailValidator, addAccount)
+  const signUpController = new SignUpController(emailValidator, addAccount)
+  const errorLogRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, errorLogRepository)
 }
