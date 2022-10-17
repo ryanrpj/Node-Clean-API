6d676543e9f982d0a8fc3f6408cbfbaba1b5ecc2
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-17 17:00:12 -0300

    feat: ensure LogMongoRepository inserts an error log into the errors collection

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,9 @@
+import { ErrorLogRepository } from '../../../../data/protocols/error-log-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class LogMongoRepository implements ErrorLogRepository {
+  async logError (stack: string): Promise<void> {
+    const collection = MongoHelper.getCollection('logs-errors')
+    await collection.insertOne({ stack, at: new Date().toISOString() })
+  }
+}
