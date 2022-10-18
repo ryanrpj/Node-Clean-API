9f76eb1c099e2c47d3a83fa1a112972af13e4f10
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-17 21:37:50 -0300

    test: ensure ExpressRouteAdapter returns error message when status code is not 2xx

--- src/main/adapters/ExpressRouteAdapter.spec.ts
+++ src/main/adapters/ExpressRouteAdapter.spec.ts
@@ -2,6 +2,7 @@ import Controller from '../../presentation/protocols/Controller'
 import HttpRequest from '../../presentation/protocols/HttpRequest'
 import HttpResponse from '../../presentation/protocols/HttpResponse'
 import ExpressRouteAdapter from './ExpressRouteAdapter'
+import HttpHelper from '../../presentation/helpers/HttpHelper'
 
 class ControllerStub implements Controller {
   async handle (request: HttpRequest): Promise<HttpResponse> {
@@ -9,7 +10,20 @@ class ControllerStub implements Controller {
   }
 }
 
-const makeSut = (): any => ExpressRouteAdapter(new ControllerStub())
+interface SutTypes {
+  sut: any
+  controller: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controller = new ControllerStub()
+  const expressRouteAdapter = ExpressRouteAdapter(controller)
+
+  return {
+    controller,
+    sut: expressRouteAdapter
+  }
+}
 
 const makeResponse = (): any => ({
   statusCode: 0,
@@ -28,7 +42,7 @@ const makeResponse = (): any => ({
 
 describe('Express Route Adapter', () => {
   test('Should return whatever controller returns when status code is 2xx', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const response = makeResponse()
 
@@ -36,5 +50,22 @@ describe('Express Route Adapter', () => {
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toMatchObject({ ok: 'ok' })
+  })
+
+  test('Should return error message when controller returns any status code other than 2xx', async () => {
+    const { sut, controller } = makeSut()
+
+    const response = makeResponse()
+
+    const error = new Error()
+    error.message = 'any_message'
+
+    const handleSpy = jest.spyOn(controller, 'handle')
+    handleSpy.mockReturnValueOnce(Promise.resolve(HttpHelper.badRequest(error)))
+
+    await sut({} as any, response)
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toMatchObject({ error: 'any_message' })
   })
 })
