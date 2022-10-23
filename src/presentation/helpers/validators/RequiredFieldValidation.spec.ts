2ccb680cbc701222bbd667f151be1d14c7898fdd
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-23 14:17:26 -0300

    test: ensure RequiredFieldValidation returns an error when field is absent

--- src/presentation/helpers/validators/RequiredFieldValidation.spec.ts
+++ src/presentation/helpers/validators/RequiredFieldValidation.spec.ts
@@ -1,5 +1,6 @@
 import Validation from './Validation'
 import RequiredFieldValidation from './RequiredFieldValidation'
+import MissingParamError from '../../errors/MissingParamError'
 
 interface SutTypes {
   sut: Validation
@@ -10,10 +11,17 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Required Field Validator', () => {
-  test('Should not return error when is present', () => {
+  test('Should not return error when field is present', () => {
     const { sut } = makeSut()
     const error = sut.validate({ any_field: 'isPresent' })
 
     expect(error).toBe(null)
+  })
+
+  test('Should return error when field is absent', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ })
+
+    expect(error).toEqual(new MissingParamError('any_field'))
   })
 })
