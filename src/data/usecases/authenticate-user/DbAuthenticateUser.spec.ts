b520a4314794a06fee5fee46baca28e88d419b59
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-23 19:56:57 -0300

    test: ensure DbAuthenticateUser calls HashComparer with correct password and hash

--- src/data/usecases/authenticate-user/DbAuthenticateUser.spec.ts
+++ src/data/usecases/authenticate-user/DbAuthenticateUser.spec.ts
@@ -2,19 +2,22 @@ import AuthenticateUser from '../../../domain/usecases/AuthenticateUser'
 import GetAccountByEmailRepository from '../../protocols/db/GetAccountByEmailRepository'
 import AccountModel from '../../../domain/models/Account'
 import DbAuthenticateUser from './DbAuthenticateUser'
+import HashComparer from '../../protocols/criptography/HashComparer'
 
 interface SutTypes {
   sut: AuthenticateUser
   getAccountByEmailRepository: GetAccountByEmailRepository
+  hashComparer: HashComparer
 }
 
 const makeGetAccountByEmailRepositoryStub = (): GetAccountByEmailRepository => {
   class GetAccountByEmailRepositoryStub implements GetAccountByEmailRepository {
-    async get (email: string): Promise<AccountModel> {
+    async get (_: string): Promise<AccountModel> {
       return {
         id: 'any_id',
         name: 'any_name',
-        email: 'any_email'
+        email: 'any_email',
+        password: 'hashed_password'
       }
     }
   }
@@ -22,11 +25,22 @@ const makeGetAccountByEmailRepositoryStub = (): GetAccountByEmailRepository => {
   return new GetAccountByEmailRepositoryStub()
 }
 
+const makeHashComparerStub = (): HashComparer => {
+  class HashComparerStub implements HashComparer {
+    compare (_: string, __: string): boolean {
+      return true
+    }
+  }
+
+  return new HashComparerStub()
+}
+
 const makeSut = (): SutTypes => {
   const getAccountByEmailRepository = makeGetAccountByEmailRepositoryStub()
-  const sut = new DbAuthenticateUser(getAccountByEmailRepository)
+  const hashComparer = makeHashComparerStub()
+  const sut = new DbAuthenticateUser(getAccountByEmailRepository, hashComparer)
 
-  return { sut, getAccountByEmailRepository }
+  return { sut, getAccountByEmailRepository, hashComparer }
 }
 
 describe('DbAuthenticateUser', () => {
@@ -48,5 +62,14 @@ describe('DbAuthenticateUser', () => {
 
     const promise = sut.auth({ email: 'any_email', password: 'any_password' })
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call HashComparer with correct password and hash', async () => {
+    const { sut, hashComparer } = makeSut()
+
+    const compareSpy = jest.spyOn(hashComparer, 'compare')
+
+    await sut.auth({ email: 'any_email', password: 'any_password' })
+    expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
   })
 })
