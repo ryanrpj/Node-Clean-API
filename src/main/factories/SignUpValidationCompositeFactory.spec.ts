2804ebbb67e8f8be8d1eb19911a748af89601936
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-23 15:22:20 -0300

    test: ensure SignUpValidationCompositeFactory passes all required validations to ValidationComposite

---
+++ src/main/factories/SignUpValidationCompositeFactory.spec.ts
@@ -0,0 +1,30 @@
+import ValidationComposite from '../../presentation/helpers/validators/ValidationComposite'
+import SignUpValidationCompositeFactory from './SignUpValidationCompositeFactory'
+import RequiredFieldValidation from '../../presentation/helpers/validators/RequiredFieldValidation'
+import Validation from '../../presentation/helpers/validators/Validation'
+import EmailValidation from '../../presentation/helpers/validators/EmailValidation'
+import EmailValidator from '../../presentation/protocols/EmailValidator'
+
+jest.mock('../../presentation/helpers/validators/ValidationComposite')
+
+class EmailValidatorStub implements EmailValidator {
+  isValid (email: string): boolean {
+    return true
+  }
+}
+
+describe('SignUp ValidationComposite Factory', () => {
+  test('Should build composite with all necessary validations', () => {
+    SignUpValidationCompositeFactory.makeSignUpValidationComposite()
+
+    const validations: Validation[] = []
+
+    for (const field of ['name', 'email', 'password']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+    validations.push(new EmailValidation('email', new EmailValidatorStub()))
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
