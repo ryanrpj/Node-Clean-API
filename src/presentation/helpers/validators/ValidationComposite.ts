8e4edec637f45d9f6f3ffaac20bc5d26b231e9c3
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-23 15:05:29 -0300

    feat: ensure ValidationComposite returns an error if any validation failed

--- src/presentation/helpers/validators/ValidationComposite.ts
+++ src/presentation/helpers/validators/ValidationComposite.ts
@@ -4,6 +4,14 @@ export default class ValidationComposite implements Validation {
   constructor (private readonly validations: Validation[]) {}
 
   validate (input: any): Error | null {
+    for (const validation of this.validations) {
+      const error = validation.validate(input)
+
+      if (error) {
+        return error
+      }
+    }
+
     return null
   }
 }
