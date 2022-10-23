7a948befa08c68895aaf1bf6355c817d68eed3d8
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-23 14:16:22 -0300

    feat: ensure RequiredFieldValidation returns an error when field is absent

--- src/presentation/helpers/validators/RequiredFieldValidation.ts
+++ src/presentation/helpers/validators/RequiredFieldValidation.ts
@@ -1,9 +1,10 @@
 import Validation from './Validation'
+import MissingParamError from '../../errors/MissingParamError'
 
 export default class RequiredFieldValidation implements Validation {
   constructor (private readonly fieldName: string) {}
 
   validate (input: any): Error | null {
-    return null
+    return input[this.fieldName] ? null : new MissingParamError(this.fieldName)
   }
 }
