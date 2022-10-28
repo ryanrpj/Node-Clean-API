8dcbe07245390351a52342143079156a4d95ca8b
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-28 20:38:06 -0300

    test: ensure JwtAdapter's encrypt returns a token if jwt's encrypt returns a token

---
+++ src/infra/criptography/JwtAdapter.spec.ts
@@ -0,0 +1,36 @@
+import JwtAdapter from './JwtAdapter'
+import jwt from 'jsonwebtoken'
+
+jest.mock('jsonwebtoken', () => ({
+  sign (data: any, encrypted: any, options: any, callback: any): void {
+    callback(null, 'any_token')
+  }
+}))
+
+interface SutTypes {
+  sut: JwtAdapter
+}
+
+const makeSut = (): SutTypes => {
+  return {
+    sut: new JwtAdapter('dummy_secret')
+  }
+}
+
+describe('JwtAdapter', () => {
+  test('Should call sign with correct values', async () => {
+    const { sut } = makeSut()
+
+    const signSpy = jest.spyOn(jwt, 'sign')
+
+    await sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'dummy_secret', expect.anything(), expect.anything())
+  })
+
+  test('Should return a token if jwt sign returns a token', async () => {
+    const { sut } = makeSut()
+
+    const token = await sut.encrypt('any_id')
+    expect(token).toBe('any_token')
+  })
+})
