e1eab3c119a84051328cc44382fcb3b97087e67f
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-10-28 21:33:21 -0300

    refactor: rename SignUpRoutes to AuthenticationRoutes

--- src/main/routes/AuthenticationRoutes.ts
+++ src/main/routes/AuthenticationRoutes.ts
@@ -2,8 +2,8 @@ import { Router } from 'express'
 import SignUpControllerFactory from '../factories/signup/SignUpControllerFactory'
 import ExpressRouteAdapter from '../adapters/ExpressRouteAdapter'
 
-const registerSignUpRoutes = (router: Router): void => {
+const authenticationRoutes = (router: Router): void => {
   router.post('/signup', ExpressRouteAdapter(SignUpControllerFactory.makeSignUpController()))
 }
 
-export default registerSignUpRoutes
+export default authenticationRoutes
