229d92d7acd52d199d2dd38ccece8de9b897df57
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-11-03 19:46:40 -0300

    fix: routes import on compiled code

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,13 +1,15 @@
 import { Application, Router } from 'express'
-import * as fs from 'fs'
+import { readdirSync } from 'fs'
 import path from 'path'
 
 export const setupRoutes = (app: Application): void => {
   const router = Router()
   app.use('/api', router)
 
-  const routesPath = path.resolve(__filename, '..', '..', 'routes')
-  const routesFiles = fs.readdirSync(routesPath).filter(fileName => fileName.endsWith('Routes.ts'))
-
-  routesFiles.forEach(async fileName => (await import(path.join(routesPath, fileName))).default(router))
+  const routesPath = path.resolve(__dirname, '..', 'routes')
+  readdirSync(routesPath).map(async file => {
+    if (!file.includes('.test.')) {
+      (await import(path.join(routesPath, file))).default(router)
+    }
+  })
 }
