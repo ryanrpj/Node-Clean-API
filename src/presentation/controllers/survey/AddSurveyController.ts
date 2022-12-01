84afc6e71061f5bdb726bd86cff48b8a78271022
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-12-01 12:54:46 -0300

    feat: ensure AddSurveyController returns 400 if Validation returns an error

--- src/presentation/controllers/survey/AddSurveyController.ts
+++ src/presentation/controllers/survey/AddSurveyController.ts
@@ -2,6 +2,7 @@ import Controller from '../../protocols/Controller'
 import HttpRequest from '../../protocols/HttpRequest'
 import HttpResponse from '../../protocols/HttpResponse'
 import Validation from '../../protocols/Validation'
+import HttpHelper from '../../helpers/http/HttpHelper'
 
 export default class AddSurveyController implements Controller {
   constructor (
@@ -9,7 +10,12 @@ export default class AddSurveyController implements Controller {
   ) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(request.body)
+    const validationError = this.validation.validate(request.body)
+
+    if (validationError) {
+      return HttpHelper.badRequest(validationError)
+    }
+
     return null as any
   }
 }
