aa3189b4e3833b200fbab9c3bc40a41fe9baab93
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-12-05 00:09:04 -0300

    feat: ensure DbGetAccountByToken calls Decrypter with correct value

--- src/data/usecases/get-account-by-token/DbGetAccountByToken.ts
+++ src/data/usecases/get-account-by-token/DbGetAccountByToken.ts
@@ -1,13 +1,17 @@
 import GetAccountByToken from '../../../domain/usecases/authentication/GetAccountByToken'
 import AccountModel from '../../../domain/models/authentication/Account'
 import GetAccountByTokenRepository from '../../protocols/db/GetAccountByTokenRepository'
+import Decrypter from '../../protocols/criptography/Decrypter'
 
 export default class DbGetAccountByToken implements GetAccountByToken {
   constructor (
+    private readonly decrypter: Decrypter,
     private readonly getAccountByTokenRepository: GetAccountByTokenRepository
   ) {}
 
   async getByToken (token: string, role: string | undefined): Promise<AccountModel | null> {
+    await this.decrypter.decrypt(token)
+
     const account = await this.getAccountByTokenRepository.getByToken(token, role)
 
     if (!account) {
