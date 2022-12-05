7154ef4d3c8a25e66468b48d736c024bc3b4492a
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-12-04 23:03:51 -0300

    test: ensure AuthenticationMiddleware calls GetAccountByToken with correct values

--- src/presentation/middlewares/AuthenticationMiddleware.spec.ts
+++ src/presentation/middlewares/AuthenticationMiddleware.spec.ts
@@ -1,16 +1,38 @@
 import AuthenticationMiddleware from './AuthenticationMiddleware'
 import HttpHelper from '../helpers/http/HttpHelper'
 import ForbiddenError from '../errors/ForbiddenError'
+import HttpRequest from '../protocols/HttpRequest'
+import GetAccountByToken from '../../domain/usecases/authentication/GetAccountByToken'
+import AccountModel from '../../domain/models/authentication/Account'
+
+const makeFakeAccount = (): AccountModel => ({
+  name: 'valid_name',
+  email: 'valid_email',
+  password: 'hashed_password',
+  id: 'valid_id'
+})
+
+class GetAccountByTokenStub implements GetAccountByToken {
+  async getByToken (token: string, role: string | undefined): Promise<AccountModel | null> {
+    return makeFakeAccount()
+  }
+}
 
 interface SutTypes {
   sut: AuthenticationMiddleware
+  getAccountByToken: GetAccountByToken
 }
 
-const makeSut = (): SutTypes => {
-  const sut = new AuthenticationMiddleware()
+const makeSut = (role?: string): SutTypes => {
+  const getAccountByToken = new GetAccountByTokenStub()
+  const sut = new AuthenticationMiddleware(getAccountByToken, role)
 
-  return { sut }
+  return { sut, getAccountByToken }
 }
+
+const makeHttpRequest = (): HttpRequest => ({
+  headers: { 'x-access-token': 'any_token' }
+})
 
 describe('Authentication Middleware', () => {
   test('Should return 403 if x-access-token is absent from request header', async () => {
@@ -20,5 +42,16 @@ describe('Authentication Middleware', () => {
     const response = await sut.handle(httpRequest)
 
     expect(response).toEqual(HttpHelper.forbidden(new ForbiddenError()))
+  })
+
+  test('Should call GetAccountByToken with correct values', async () => {
+    const role = 'any_role'
+    const { sut, getAccountByToken } = makeSut(role)
+    const getByTokenSpy = jest.spyOn(getAccountByToken, 'getByToken')
+    const httpRequest = makeHttpRequest()
+
+    await sut.handle(httpRequest)
+
+    expect(getByTokenSpy).toHaveBeenCalledWith('any_token', 'any_role')
   })
 })
