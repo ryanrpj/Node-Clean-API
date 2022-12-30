090a376ca5380038997c1be30e9b1deae98dd585
Ryan Ribeiro <47592751+ryanrpj@users.noreply.github.com>
2022-12-30 16:01:25 -0300

    test: ensure add survey route returns 403 if no token is provided

--- src/main/routes/SurveyRoutes.test.ts
+++ src/main/routes/SurveyRoutes.test.ts
@@ -3,6 +3,20 @@ import app from '../config/app'
 import MongoHelper from '../../infra/db/mongodb/helpers/MongoHelper'
 import env from '../config/env'
 import { Collection } from 'mongodb'
+import { AddSurveyModel } from '../../domain/usecases/survey/AddSurvey'
+
+const makeSurvey = (): AddSurveyModel => ({
+  question: 'What should I write here?',
+  answers: [
+    {
+      image: 'js-logo.com',
+      answer: 'JavaScript is awesome!'
+    },
+    {
+      answer: 'But TypeScript is fancier!'
+    }
+  ]
+})
 
 describe('SignUp Routes', () => {
   let collection: Collection
@@ -16,21 +30,10 @@ describe('SignUp Routes', () => {
     await collection.deleteMany({})
   })
 
-  test('Should return 201 on add survey success', async () => {
+  test('Should return 403 on add survey without Bearer token', async () => {
     await request(app)
       .post('/api/surveys')
-      .send({
-        question: 'What should I write here?',
-        answers: [
-          {
-            image: 'js-logo.com',
-            answer: 'JavaScript is awesome!'
-          },
-          {
-            answer: 'But TypeScript is fancier!'
-          }
-        ]
-      })
-      .expect(201)
+      .send(makeSurvey())
+      .expect(403)
   })
 })
